0ffbf181bad8046caedf41fe5bef2a508e7d85b0
sy0419 <psy041912@gmail.com>
2025-01-22 23:10:51 +0900

    Different type of Number data

---
+++ dateNumberType.py
@@ -0,0 +1,9 @@
+int_data = 10
+# Decimal number 정수형 상수
+bin_data = 0b10
+# Bianary number 2진수
+oct_data = 0o10
+# Octal number 8진수
+hex_data = 0x10
+# hexadecimal number 16진수
+long = 123456789123456789
--- typeOfNumber.py
+++
@@ -1,4 +0,0 @@
-int_data = 10
-# integer 정수형 상수
-bin_data = 0b10
-# bianary 이진수
