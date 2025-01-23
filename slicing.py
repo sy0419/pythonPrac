89fbce1ca82f78a682896e9335bd7941cffb89b2
sy0419 <psy041912@gmail.com>
2025-01-23 15:56:32 +0900

    Slicing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+strdata = 'Time is money!!'
+print(strdata[1:5])
+print(strdata[:7])
+print(strdata[9:])
+print(strdata[:-3])
+print(strdata[-3:])
+print(strdata[:])
+print(strdata[::2])
+
+# Start index <= [start index : end index] < end index
