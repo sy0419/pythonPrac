28974871f2c52c11af63322b108a0fd4395a8a49
sy0419 <psy041912@gmail.com>
2025-01-23 17:36:13 +0900

    Connection of Sequence data type

---
+++ connectionOfSequenceData.py
@@ -0,0 +1,5 @@
+strdatal = 'I love '; strdata2 = 'Python'; strdata3 = 'you'
+listdata1 = [1, 2, 3]; listdata2 = [4, 5, 6]
+print(strdatal + strdata2)
+print(strdatal + strdata3)
+print(listdata1 + listdata2)
