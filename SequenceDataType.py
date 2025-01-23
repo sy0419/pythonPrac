1338d042c338c8b32e11c5eaee6ef16d4a9202b2
sy0419 <psy041912@gmail.com>
2025-01-23 15:48:38 +0900

    Sequence Data Types

---
+++ SequenceDataType.py
@@ -0,0 +1,6 @@
+strdata = 'abcde'
+# 1.String data is Sequence data type
+listdata = [1, [2, 3], 'Hi']
+# 2. List data is Sequence data type
+tupledata = (100, 200, 300)
+# 3. Tuple data is Sequence data type
