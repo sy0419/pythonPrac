2c196157e504c57aea40ae7849bcb7448a6270cb
sy0419 <psy041912@gmail.com>
2025-01-25 01:52:07 +0900

    Calculator program

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+def plus(a, b):
+    print(a + b)
+
+def minus(a, b):
+    print(a - b)
+
+def multiplication(a, b):
+    print(a * b)
+
+def divide(a, b):
+    print(a / b)
+
+def power(a=0, b=1):
+   print(a ** b)
+
+def remainder(a=0, b=1):
+   print(a % b)
+
+plus(10, 5)
+minus(5, 3)
+multiplication(2, 3)
+divide(6, 3)
+power(2, 2)
+remainder(6, 4)
+
