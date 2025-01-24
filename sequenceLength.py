37044448637b208c709f779f051e493e4838abf8
sy0419 <psy041912@gmail.com>
2025-01-24 12:50:42 +0900

    Sequence data has fixed length

---
+++ sequenceLength.py
@@ -0,0 +1,7 @@
+strdata1 = 'I love Python'
+strdata2 = 'I like Java'
+listdata = ['a', 'b', 'c', strdata1, strdata2]
+print(len(strdata1))
+print(len(strdata2))
+print(len(listdata))
+print(len(strdata1[3]))
