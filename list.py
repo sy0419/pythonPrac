035209b6f2226d41ece0edbf92d365b345aa538a
sy0419 <psy041912@gmail.com>
2025-01-24 13:23:06 +0900

    Using List.

---
+++ list.py
@@ -0,0 +1,11 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = ['a', 'b', 'c']
+list3 = [1, 'a', 'abc', [1, 2, 3, 4, 5], ['a', 'b', 'c']]
+list1[0] = 6
+print(list1)
+def myfunc():
+    print('Hello')
+list4 = [1, 2, myfunc]
+list4[2]()
+
+# List is the most fuction to use as Sequence data.
