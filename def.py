3714217a842418440a73dc3b7a3f5c57dffea179
sy0419 <psy041912@gmail.com>
2025-01-24 13:56:01 +0900

    Using function.

---
+++ def.py
@@ -0,0 +1,17 @@
+def add_number(n1, n2):
+    ret = n1 + n2
+    #print(ret)
+    return ret
+
+
+def add_txt(t1, t2):
+    print(t1 + t2)
+
+ans = add_number(10, 15)
+print(ans)
+
+text1 = 'Republic of Korea~'
+text2 = 'Fighting!'
+add_txt(text1, text2)
+
+# Function is the set which has purpose and it can make as independently.
