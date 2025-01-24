c09039cdd6d29ebee060fe40c09c55e4e120ae3d
sy0419 <psy041912@gmail.com>
2025-01-24 13:36:42 +0900

    using Dictionary.

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+dict1 = {'a':1, 'b':2, 'c':3}
+print(dict1['a'])
+dict1['d'] = 4
+print(dict1)
+dict1['b'] = 7
+print(dict1)
+print(len(dict1))
+
+# Dictionary is a pair as Key and Value.
+# Dictionary length is counting date section which divided with coma.
