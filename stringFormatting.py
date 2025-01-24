40287c54f87c709c968ecc30480bbd5f0a9eba4a
sy0419 <psy041912@gmail.com>
2025-01-24 13:08:40 +0900

    String farmatting is using for changable values without typing everytimes.

---
+++ stringFormatting.py
@@ -0,0 +1,8 @@
+txt1 = 'Java'; txt2 = 'Python'
+num1 = 5; num2 = 10
+print('I am more familiar to %s than %s.' %(txt1, txt2))
+print('%s is easier than %s as %d times' %(txt1, txt2, num1))
+print('%d + %d = %d' %(num1, num2, num1 + num2))
+print('Grobal economic growth rate on this year is raised as %d%% compare to last year' %num1)
+
+# %s string, %c string or one sign, %f real number, %d integer, %% sign %
