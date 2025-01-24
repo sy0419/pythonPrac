7897fd74a87edb3a715419f99dcb2931d19a7c0c
sy0419 <psy041912@gmail.com>
2025-01-24 13:18:23 +0900

    Understanding of Escape signs.

---
+++ escapeSign.py
@@ -0,0 +1,7 @@
+print('I like python. \nPython is easier than Java.')
+print('Name: John Smith\tSex: Male\tAge: 22')
+print('This sentence length is too long \
+      so using \Enter key to show much better readable length.')
+print('Single quotation mark(\') and Double quotation mark(") is using for definition.')
+
+# \n Changed line, \t Tab, \Enter Keep to the next line, \' or \" Sign of ' and Sign of "
