55763204879036f9f6536f96e39206a79486d36c
sy0419 <psy041912@gmail.com>
2025-01-24 14:25:14 +0900

    Using Function.

--- def.py
+++ def.py
@@ -15,3 +15,26 @@ text2 = 'Fighting!'
 add_txt(text1, text2)
 
 # Function is the set which has purpose and it can make as independently.
+# i) declare function with value and reture value.
+#   def def_name(v1, v2):
+#      code
+#      code
+#      return result
+#  ㄴ call function
+#     variable = def_name(value1, value2, ...)
+#
+# ii) declare function with return value.
+#     def def_name():
+#       code
+#       code
+#       return result
+#
+# iii) declare function without value and return value.
+#      def def_name():
+#        code
+#        code
+#        return (or skip)
+#  ㄴ call function which does not have return value
+#     def_name(value1, value2, ...)
+#  ㄴ call function which does not have value and return value
+#     def_name()
