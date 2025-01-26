6f33bf05f92f03244d1efcfd375bca6d6a528f91
sy0419 <psy041912@gmail.com>
2025-01-26 12:26:33 +0900

    Random number picking game program

--- RandomGame.py
+++ RandomGame.py
@@ -1,13 +1,33 @@
+"""
 from random import randint
 
 print("Welcome to the game.")
 
+pc_choice = randint(1, 50)
 user_choic = int(input("Input random number."))
-pc_choic = randint(1, 50)
 
-if user_choic == pc_choic:
+if user_choice == pc_choice:
     print("You won.")
-elif user_choic < pc_choic:
+elif user_choice > pc_choice:
     print("Lower.")
-elif user_choic > pc_choic:
+elif user_choice < pc_choice:
     print("Higher.")
+"""
+
+
+from random import randint
+
+print("Welcome to the game.")
+pc_choice = randint(1, 50)
+
+playing = True
+
+while playing:
+  user_choice = int(input("Input random number."))
+  if user_choice == pc_choice:
+      print("You won.")
+      playing = False
+  elif user_choice > pc_choice:
+      print("Lower.")
+  elif user_choice < pc_choice:
+      print("Higher.")
