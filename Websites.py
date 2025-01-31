5633bd5ccbe67c20abb324406c6fd66114c12706
sy0419 <psy041912@gmail.com>
2025-01-31 11:59:42 +0900

    Program that checking website's status

--- Websites.py
+++ Websites.py
@@ -1,12 +1,24 @@
+from requests import get
+
 websites = (
     "www.google.com",
     "https://www.naver.com",
-    "www.daum.com",
+    "www.daum.net",
     "https://www.nate.com",
-    "www.python.com"
+    "www.python.org"
 )
+
+results = {
+
+}
 
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+    
+print(results)
