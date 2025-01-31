94341219e4731b738ad5b1ca14e33ffd4d9c4591
sy0419 <psy041912@gmail.com>
2025-01-31 12:11:17 +0900

    Program that checking the website's statu

--- Websites.py
+++ Websites.py
@@ -16,8 +16,12 @@ for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
     response = get(website)
-    if response.status_code == 200:
+    if response.status_code >=200 and response.status_code < 300:
         results[website] = "OK"
+    elif response.status_code >= 300 and response.status_code < 400:
+        results[website] = "REDIRECT"
+    elif response.status_code >= 400 and response.status_code < 500:
+        results[website] = "ERROR"
     else:
         results[website] = "FAILED"
     
