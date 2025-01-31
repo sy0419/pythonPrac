cb30c31f7287b30f840218d5dc990b909aa2da03
sy0419 <psy041912@gmail.com>
2025-01-31 14:38:27 +0900

    Program that inditation of pet's informations

--- Pet.py
+++ Pet.py
@@ -5,11 +5,14 @@ class Dog():
         self.age = age
 
 class Puppy(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, breed, 2)
+
     def __str__(self):
         return f"{self.breed} puppy name is {self.name} and I am {self.age}months years old."
     
-ruffss = Puppy(name = "Ruffss", breed = "Beagle", age = 1)
-bibi = Puppy(name = "Bibi", breed = "Dalmatian", age = 3)
+ruffss = Puppy(name = "Ruffss", breed = "Beagle")
+bibi = Puppy(name = "Bibi", breed = "Dalmatian")
 
 print(ruffss)
 print(bibi)
