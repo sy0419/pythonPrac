304eb590f5afc6fa364c9b0d2548408734143e3c
sy0419 <psy041912@gmail.com>
2025-02-01 16:25:02 +0900

    Program for ordered drink

---
+++ OrderDrinks(based on Pet).py
@@ -0,0 +1,25 @@
+class OrderDrink():
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
+
+class Bavarage(OrderDrink):
+    def __init__(self, name):
+        super().__init__(name, "Bavarage")
+
+    def __str__(self):
+        return f"My choice is {self.kind}/{self.name}"
+    
+class Alcohol(OrderDrink):
+    def __init__(self, name):
+        super().__init__(name, "Alcohol")
+
+    def __str__(self):
+        return f"My selection is {self.kind}/{self.name}"
+
+
+
+order1 = Bavarage(name = "Iced tea")
+order2 = Alcohol(name = "Cass")
+print(order1)
+print(order2)
