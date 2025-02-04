b25725a1415dfb3256b611a3f4d0ddb6b2e33fed
sy0419 <psy041912@gmail.com>
2025-02-04 15:33:54 +0900

    The basic game program

--- Game.py
+++ Game.py
@@ -7,6 +7,9 @@ class Player():
     def introduce(self):
         print(f"Hello, My name is {self.name} and I am in {self.team}")
 
+    def team_members(self):
+        print(f"{self.team}: {self.name}")
+
 class Team():
     def __init__(self, team_name):
         self.team_name = team_name
@@ -15,6 +18,16 @@ class Team():
     def show_player(self):
         for player in self.players:
             player.introduce()
+
+    def show_team_members(self):
+        for player in self.players:
+            player.team_members()
+
+    def team_total_xp(self):
+        team_total_xp = 0
+        for player in self.players:
+            team_total_xp += player.xp
+        print(f"{self.team_name}'s total xp: {team_total_xp}")
     
     def add_player(self, name):
         new_player = Player(name, self.team_name)
@@ -24,6 +37,7 @@ class Team():
         for player in self.players:
             if player.name == name:
                 self.players.remove(player)
+                return print(f"{name} is not in {self.team_name} anymore.")
 
 
 team_X = Team("Team X")
@@ -32,14 +46,22 @@ team_Y = Team("Team Y")
 team_X.add_player("Harry")
 team_X.add_player("James")
 team_X.add_player("Leo")
+team_X.add_player("Sam")
+team_X.add_player("Tim")
 
 team_Y.add_player("Anna")
 team_Y.add_player("Jane")
+team_Y.add_player("Julia")
 
-team_X.show_player()
-team_Y.show_player()
-print("------------------")
 team_X.remove_player("Leo")
 
-team_X.show_player()
-team_Y.show_player()
+# team_X.show_player()
+# team_Y.show_player()
+
+team_X.show_team_members()
+print("---------------------------")
+team_Y.show_team_members()
+
+print("---------------------------")
+team_X.team_total_xp()
+team_Y.team_total_xp()
