c05f8b6241c4b764a93a78270206dfd9457e854d
sy0419 <psy041912@gmail.com>
2025-02-04 23:47:33 +0900

    Program to bring data from url and using the data

--- JobScraper.py
+++ JobScraper.py
@@ -9,6 +9,25 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 # jobs = soup.find("section", id="category-18")
 
-jobs = soup.find("section", class_="jobs").find_all("li")
+jobs = soup.find("section", class_="jobs").find_all("li")[:-1]
+all_jobs = []
 
-print(jobs)
+for job in jobs:
+    try:
+        title = job.find("span", class_="title").text
+        company, contract_type, region = job.find_all("span", class_="company")
+        try:
+            url = job.find('div', class_ = 'tooltip--flag-logo').next_sibling['href']
+        except KeyError:
+            url = 'You need log-in'
+        job_data = {
+            "title": title,
+            "company": company.text,
+            "contract_type": contract_type.text,
+            "region": region.text,
+            "url": f"https://weworkremotely.com{url}"
+        }
+        all_jobs.append(job_data)
+    except:
+        pass
+print(all_jobs)
